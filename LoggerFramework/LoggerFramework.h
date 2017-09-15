8495edef990e2fa817b54b1cbc50f7800310eefc
sujay huilgol <sujayhuilgol@sujays-macbook-pro.local>
2017-09-15 12:57:13 +0530

    Initial Commit

---
+++ LoggerFramework/LoggerFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoggerFramework.h
+//  LoggerFramework
+//
+//  Created by sujay huilgol on 9/15/17.
+//  Copyright © 2017 sujay huilgol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoggerFramework.
+FOUNDATION_EXPORT double LoggerFrameworkVersionNumber;
+
+//! Project version string for LoggerFramework.
+FOUNDATION_EXPORT const unsigned char LoggerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggerFramework/PublicHeader.h>
+
+
